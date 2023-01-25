d3a5d87b8ce040bee2caa665bd943e564223d802
Pavel <pavgrebens@mail.ru>
2023-01-25 22:05:18 +0300

    Edit label input

--- Algorithms/task_0.py
+++ Algorithms/task_0.py
@@ -1,5 +1,5 @@
 def task_1():
-    numbers_list = input("Ввод через чисел через пробел: ")
+    numbers_list = input("Ввод чисел через пробел: ")
     numbers_list = numbers_list.split(" ")
 
     number_list_map_object = list(map(int, numbers_list))
@@ -14,16 +14,16 @@ task_1()
 
 def task_2():
     numbers_list = [1, 5, -10, 3, 5, -32]
-    empty_numbers_list = []
+    empty_numbers_list_pos = []
     empty_numbers_list_negative = []
     
     for i in numbers_list:
         if i > 0:
-            empty_numbers_list.append(i)
+            empty_numbers_list_pos.append(i)
         else:
             empty_numbers_list_negative.append(i)
             
-    print(f"Положительные числа: {empty_numbers_list}")
+    print(f"Положительные числа: {empty_numbers_list_pos}")
     print(f"Отрицательные числа: {empty_numbers_list_negative}")
     
 task_2()
