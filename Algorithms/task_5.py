2494a939b17ab9847f3f3dbf31740c64c906aa54
Pavel <pavgrebens@mail.ru>
2023-01-25 22:03:25 +0300

    Fixed task_5 and added requirements.txt

--- Algorithms/task_5.py
+++ Algorithms/task_5.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from pprint import pprint
 
 def use_api():
@@ -9,7 +10,19 @@ def use_api():
    if response.status_code == 200:
       with open("get_json_data.txt", encoding="utf-8", mode="w+") as file:
            data = response.json()
-           file.dump(data["company"], data["id"], data["email"], data["creat_at"], data["name"],data["url"]) 
+
+
+           data_items = {
+               "data": {
+                  "id": data["id"],
+                  "created_at": data["created_at"],
+                  "name": data["name"],
+                  "url": data["url"],
+               }
+           }
+
+           json.dump(data_items, file)
+
    else:
       return "Соединение не удалось"
 
